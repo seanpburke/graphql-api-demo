2d3218b63a3ac3d89f90a34b2c790eef6aadb83e
Sean Burke <sean@xenadyne.com>
2020-08-28 13:09:19 -0700

    Add access logging.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/aws/aws-sdk-go v1.30.19
+	github.com/gorilla/handlers v1.5.0
 	github.com/graph-gophers/graphql-go v0.0.0-20200309224638-dae41bde9ef9
 	github.com/mweagle/Sparta v1.15.0
 	github.com/sirupsen/logrus v1.6.0
--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/graph-gophers/graphql-go/relay"
 	"github.com/sburke-at-ziprecruiter/graphql-api-demo/pkg/schema"
 )
 
 func main() {
 	http.HandleFunc("/", func(_ http.ResponseWriter, _ *http.Request) {}) // Handle health checks from the load balancer
-	http.Handle("/query", &relay.Handler{Schema: schema.Schema})
+	http.Handle("/query", handlers.LoggingHandler(os.Stdout, &relay.Handler{Schema: schema.Schema}))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
